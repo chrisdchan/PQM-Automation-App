d3e09d24f59826f41dde68b8c40cf5ea5a7ddc13
Chris <christopherdchan02@gmail.com>
2023-01-04 23:52:24 -0500

    Add Error String Visibility Converter

---
+++ PQM-V2/Converters/ErrorStringVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PQM_V2.Converters
+{
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    public class ErrorStringVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility))
+                throw new InvalidOperationException("The target must be a Visibility");
+
+            return (string.IsNullOrEmpty((string)value)) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+}
